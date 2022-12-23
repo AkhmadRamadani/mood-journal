187bfd75eac01e79bb909b95ce98986f044aafac
oocengg <fauzinugraha06@gmail.com>
2022-12-23 13:17:09 +0700

    Add privacy police page

---
+++ lib/modules/profile/views/privacy_police.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+import 'package:moodie/shared/themes/colors.dart';
+import 'package:moodie/shared/themes/help_text.dart';
+import 'package:moodie/shared/themes/privacy_police_text.dart';
+import 'package:moodie/shared/themes/radius.dart';
+import 'package:moodie/shared/themes/spacing.dart';
+import 'package:get/get.dart';
+import 'package:moodie/shared/widgets/cards/bulletList_card.dart';
+
+class PrivacyPolice extends StatelessWidget {
+  const PrivacyPolice({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ThemeColor.primary,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    child: IconButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: ThemeColor.white,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'Privacy & Police',
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                          color: ThemeColor.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: Spacing.spacing * 2,
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(Spacing.spacing * 3),
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: ThemeColor.background,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(CustomRadius.defaultRadius),
+                    topRight: Radius.circular(CustomRadius.defaultRadius),
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(
+                          top: Spacing.spacing,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              PrivacyText.date,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.desc1,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.desc2,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.desc3,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.cu,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.descu,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.cu1),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.cu2),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.cu3),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.descu2,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.si,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.dessi,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.si1),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.si2),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.si3),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.yc,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.desyc,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.yc1),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.yc2),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.yc3),
+                            const SizedBox(height: Spacing.spacing * 2),
+                            const BulletList(text: PrivacyText.yc4),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.ds,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleMedium!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                            ),
+                            const SizedBox(height: Spacing.spacing * 3),
+                            Text(
+                              PrivacyText.desds,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .titleSmall!
+                                  .copyWith(
+                                    color: ThemeColor.neutral_900,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/themes/privacy_police_text.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class PrivacyText {
+  PrivacyText._();
+
+  static const String date = 'Effective Date : Until 2025';
+
+  static const String desc1 =
+      'This Privacy Policy explains how Moodie ("we," "us," or "our") collects, uses, and shares information when you use our mobile application, Moodie - Mood Journal ("Application").';
+  static const String desc2 =
+      'By using the Application, you agree to the collection, use, and sharing of your information as described in this Privacy Policy. If you do not agree with our policies and practices, do not use the Application.';
+  static const String desc3 =
+      'We may change our Privacy Policy from time to time. We encourage you to review the Privacy Policy whenever you access the Application to stay informed about our information practices and the choices available to you.';
+
+  static const String cu = 'Collection and Use of Information';
+  static const String descu =
+      'We collect information about you in the following ways when you use the Application :';
+  static const String cu1 =
+      'Information you provide : We collect information that you enter into the Application or provide to us in some other way. This may include your name, email address, and other contact information, as well as information about your mood and any notes or observations you choose to include.';
+  static const String cu2 =
+      'Location information : We may collect information about your location when you use the Application. This may include your device\'s GPS coordinates or information about nearby WiFi networks or cell towers.';
+  static const String cu3 =
+      'Device information : We may collect information about the device you use to access the Application, including the device\'s unique identifier, IP address, and operating system.';
+  static const String descu2 =
+      'We use the information we collect to provide and improve the Application, as well as to personalize your experience. We may also use the information for research and analytics purposes, or to contact you with updates or promotional materials.';
+
+  static const String si = 'Sharing of Information';
+  static const String dessi =
+      'We may share your information with third parties in the following circumstances :';
+  static const String si1 =
+      'With service providers: We may share your information with third-party service providers who perform functions on our behalf, such as hosting, data storage, and analytics. These service providers are required to maintain the confidentiality of your information and are not allowed to use it for any other purpose.';
+  static const String si2 =
+      'With law enforcement or in response to legal requests: We may disclose your information to law enforcement or government authorities, or in response to a subpoena or other legal request, if we believe it is necessary to do so to comply with the law or to protect the rights, property, or safety of our company, our users, or others.';
+  static const String si3 =
+      'In connection with a sale or transfer of assets: If we are acquired by or merged with another company, or if we sell or transfer all or a portion of our assets, we may transfer your information to the acquiring company or the successor entity.';
+
+  static const String yc = 'Your Choices';
+  static const String desyc =
+      'You have the following choices regarding the collection, use, and sharing of your information :';
+  static const String yc1 =
+      'Opt-out of marketing communications: You can opt-out of receiving promotional emails from us by following the unsubscribe instructions contained in the email.';
+  static const String yc2 =
+      'Access and update your information: You can access and update your contact information or delete your account by logging into the Application and accessing the settings menu.';
+  static const String yc3 =
+      'Limit the information you provide: You can choose not to provide certain information to us, but doing so may limit your ability to use the Application.';
+  static const String yc4 =
+      'Enable or disable location services: You can enable or disable location services in the settings menu of your device. If you disable location services, the Application may not function properly.';
+
+  static const String ds = 'Data Security';
+  static const String desds =
+      'We have implemented measures to protect the security of your information and to prevent unauthorized access or misuse. However, no method of transmission over the internet, or method of electronic storage, is completely secure. Therefore, we cannot guarantee the absolute security of your';
+}
